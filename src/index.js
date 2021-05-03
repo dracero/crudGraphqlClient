02c1f0c6288846f244c2776e3b86f41608064ab4
dracero <diego.racero@gmail.com>
2021-05-03 18:04:29 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Edit from './components/Edit';
 import Create from './components/Create';
 import Show from './components/Show';
 
-const client = new ApolloClient({ uri: 'http://localhost:3000/graphql' });
+const client = new ApolloClient({ uri: 'https://gphsever.herokuapp.com/graphql' });
 
 ReactDOM.render(
     <ApolloProvider client={client}>
